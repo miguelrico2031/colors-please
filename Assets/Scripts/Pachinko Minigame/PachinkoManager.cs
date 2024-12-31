f52d49ee1afc1e9785934215435962c196909fd7
m4nu3lgc <loloandthebeastcactus@gmail.com>
2024-12-31 17:48:51 +0100

    pachinko minigame

--- Assets/Scripts/Pachinko Minigame/PachinkoManager.cs
+++ Assets/Scripts/Pachinko Minigame/PachinkoManager.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,27 +13,59 @@ public enum states
 
 public class PachinkoManager : MonoBehaviour
 {
-    [SerializeField] private states _pachinkoState;
+    #region Variables
+    [Header("Pachinko Params")]
     [SerializeField] private GameObject _pachinkoBall;
-    [SerializeField] private GameObject[] _pachinkoLayouts;
+    [SerializeField] private float _forceFactor = 2f;
+    [SerializeField] private float _minForce = 1f;
+    [SerializeField] private float _maxForce = 5f;
+    [SerializeField] private GameObject[] _pachinkoLayoutsList;
     [SerializeField] private GameObject _pachinkoGrid;
-    [SerializeField] private SpriteRenderer _targetColorSprite;
-    [SerializeField] private Rigidbody2D _rb;
-    [SerializeField] private Vector2 _originalPosition;
-    [SerializeField] private Vector2 _aimDirection;
-    [SerializeField] private float _forceFactor;
-    [SerializeField] private uint _ballCount;
-    [SerializeField] private byte[] _RGBComponents;
-    [SerializeField] private uint _maxBalls = 3;
+    [SerializeField] private GameObject _pachinkoEndingWall;
+    [Header("Target Color")]
+    [SerializeField]private SpriteRenderer _targetColorSprite;
+    [Header("Arrow Params")]
+    [SerializeField] private GameObject _arrow;
+    [SerializeField] private SpriteRenderer _arrowSprite;
+    [SerializeField] private float _minWidth = 4f;
+    [SerializeField] private float _maxWidth = 10f;
 
+    private states _pachinkoState;
+    private Rigidbody2D _rb;
+    private Vector2 _originalPosition;
+    private Vector2 _aimDirection;
+    private uint _ballCount;
+    private uint _maxBalls = 3;
+    private RGB255 _targetColor;
+    private byte[] _RGBComponents;
+    
+    #endregion
+
+    #region Singleton
+    public static PachinkoManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    #endregion
+
+    #region UnityFunctions
     private void Start()
     {
-        RGB255 targetColor = RGB255.Random();
-        _targetColorSprite.color = targetColor.ToColor();
+        _targetColor = RGB255.Random();
+        _targetColorSprite.color = _targetColor.ToColor();
         _pachinkoState = states.startAim;
         _ballCount = 0;
         _RGBComponents = new byte[3];
-        Instantiate(_pachinkoLayouts[Random.Range(0, _pachinkoLayouts.Length)], _pachinkoGrid.transform);
+        _arrow.SetActive(false);
+        Instantiate(_pachinkoLayoutsList[Random.Range(0, _pachinkoLayoutsList.Length)], _pachinkoGrid.transform);
     }
 
     private void Update()
@@ -46,15 +79,14 @@ public class PachinkoManager : MonoBehaviour
                 Aim();
                 break;
             case states.game:
-                Game();
-                break;
-            case states.next:
                 break;
             case states.finish:
                 break;
         }
     }
-
+    #endregion
+    
+    #region StatesFunctions
     private void StartAim()
     {
         _rb = _pachinkoBall.GetComponent<Rigidbody2D>();
@@ -70,26 +102,57 @@ public class PachinkoManager : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
             {
-                var touchPos = (Vector2)Camera.main.ScreenToWorldPoint(touch.position);
-                _aimDirection = touchPos - _originalPosition;
+                _arrow.SetActive(true);
             }
 
             if (touch.phase == TouchPhase.Moved)
             {
                 var touchPos = (Vector2)Camera.main.ScreenToWorldPoint(touch.position);
                 _aimDirection = touchPos - _originalPosition;
+                float dirLengthClamped = Mathf.Clamp(_aimDirection.magnitude, _minWidth, _maxWidth);
+                Vector2 newSize = new Vector2(dirLengthClamped + Mathf.Clamp(_aimDirection.magnitude, 0f, _maxWidth), _arrowSprite.size.y);
+                _arrowSprite.size = newSize;
+                float r = (dirLengthClamped - _minWidth) / (_maxWidth - _minWidth);
+                float g = 1 - ((dirLengthClamped - _minWidth) / (_maxWidth - _minWidth));
+                _arrowSprite.color = new Color(r,g,0f);
+                _arrow.transform.right = _aimDirection;
             }
 
             if (touch.phase == TouchPhase.Ended)
             {
+                _arrow.SetActive(false);
                 _rb.constraints = RigidbodyConstraints2D.None;
                 _rb.AddForce(_aimDirection * _forceFactor, ForceMode2D.Impulse);
+                _pachinkoState = states.game;
             }
         }
     }
 
-    private void Game()
+    public void DetectRGBValue()
     {
-
+        _rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        float value = _pachinkoBall.transform.position.x;
+        value = value + _pachinkoEndingWall.transform.localScale.x / 2;
+        value = Mathf.Clamp(value / _pachinkoEndingWall.transform.localScale.x, 0f, 1f);
+        value *= 255;
+        _RGBComponents[_ballCount] = (byte) value;
+        Debug.Log(_RGBComponents[_ballCount]);
+        _ballCount++;
+        if( _ballCount < _maxBalls)
+        {
+            ResetBall();
+            _pachinkoState = states.startAim;
+        } else
+        {
+            RGB255 guessedColor = new RGB255(_RGBComponents[0], _RGBComponents[1], _RGBComponents[2]);
+            Debug.Log(guessedColor.ToString());
+            ServiceLocator.Get<IDayService>().FinishMinigame(_targetColor, guessedColor);
+        }
     }
+
+    private void ResetBall()
+    {
+        _pachinkoBall.transform.position = _originalPosition;
+    }
+    #endregion
 }
